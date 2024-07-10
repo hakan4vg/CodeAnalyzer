152f62a31f50fd021d514de376ab54184cbb7b36
hakan <hakan@ALPSTAJ>
2024-07-10 16:12:50 +0300

    Descriptors are finalized other than possibility of minor improvements

--- CodeAnalyzer/CodeAnalyzer/CodeAnalyzerAnalyzer.cs
+++ CodeAnalyzer/CodeAnalyzer/CodeAnalyzerAnalyzer.cs
@@ -14,14 +14,26 @@ namespace CodeAnalyzer
 
     public static class DiagnosticRules
     {
-        public static DiagnosticDescriptor PascalCaseRule = new DiagnosticDescriptor(
-            id: "CA001",
-            title: "PascalCase Naming Rule",
-            messageFormat: "The name '{0}' should be in PascalCase",
-            category: "Naming",
-            defaultSeverity: DiagnosticSeverity.Warning,
-            isEnabledByDefault: true
-        );
+
+
+        /*public abstract class DescriptorBase
+        {
+            public abstract string Id { get; }
+            public abstract string Title { get; }
+            public abstract string MessageFormat { get; }
+            public abstract string Category { get; }
+            public abstract DiagnosticSeverity DefaultSeverity { get; }
+            public abstract bool IsEnabledByDefault { get; }
+
+            public DiagnosticDescriptor Rule => new DiagnosticDescriptor(
+                id: Id,
+                title: Title,
+                messageFormat: MessageFormat,
+                category: Category,
+                defaultSeverity: DefaultSeverity,
+                isEnabledByDefault: IsEnabledByDefault
+            );
+        }*/
 
         public static DiagnosticDescriptor CamelCaseRule = new DiagnosticDescriptor(
             id: "CA002",
@@ -31,7 +43,7 @@ namespace CodeAnalyzer
             defaultSeverity: DiagnosticSeverity.Warning,
             isEnabledByDefault: true
         );
-
+        
         public static DiagnosticDescriptor ConstantsRule = new DiagnosticDescriptor(
             id: "CA003",
             title: "ConstantsRule",
@@ -89,19 +101,100 @@ namespace CodeAnalyzer
         public static DiagnosticDescriptor MethodLengthRule = new DiagnosticDescriptor(
             id: "CA009",
             title: "Method Length Rule",
-            messageFormat: "Method {0} should not exceed ~70 lines",
+            messageFormat: "Method '{0}' should not exceed ~70 lines",
             category: "Code Conventions",
             defaultSeverity: DiagnosticSeverity.Info,
             isEnabledByDefault: true
         );
 
-        public static DiagnosticDescriptor UnnecessaryCurlyBraces = new DiagnosticDescriptor(
+        public static DiagnosticDescriptor UnnecessaryCurlyBracesRule = new DiagnosticDescriptor(
             id:"CA010",
             title:"Unnecessary Curly Braces Rule",
             messageFormat:"You've entered unnecessary curly braces",
             category:"Class and Method Structure",
             defaultSeverity:DiagnosticSeverity.Error,
             isEnabledByDefault:true
+        );
+
+        public static DiagnosticDescriptor PublicClassRule = new DiagnosticDescriptor(
+            id: "CA011",
+            title: "Public Class Access Modifier Rule",
+            messageFormat: "Class '{0}' should be public",
+            category: "Identifiers",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true
+        );
+
+        public static DiagnosticDescriptor PrivateMethodRule = new DiagnosticDescriptor(
+            id: "CA012",
+            title: "Private Method Modifier Rule",
+            messageFormat: "Method '{0}' should be private",
+            category: "Identifiers",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true
+        );
+
+        public static DiagnosticDescriptor PublicPropertyRule = new DiagnosticDescriptor(
+            id: "CA013",
+            title: "Public Property Modifier Rule",
+            messageFormat: "Property '{0}' should be public",
+            category: "Identifiers",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true
+        );
+
+        public static DiagnosticDescriptor CodeBlockFramingRule = new DiagnosticDescriptor(
+            id: "CA014",
+            title: "Code Block Framing Rule",
+            messageFormat: "This block should've been framed in curly braces",
+            category: "Code Scoping",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true
+        );
+
+        public static DiagnosticDescriptor NullReferenceRule = new DiagnosticDescriptor(
+            id: "CA015",
+            title: "Null Reference Rule",
+            messageFormat: "You are trying to use something that is null",
+            category: "Code Scoping",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true
+        );
+
+        public static DiagnosticDescriptor GetterRule = new DiagnosticDescriptor(
+            id: "CA016",
+            title: "Getter Rule",
+            messageFormat: "You're missing getter method",
+            category: "Encapsulation",
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true
+        );
+       
+        public static DiagnosticDescriptor SetterRule = new DiagnosticDescriptor(
+            id: "CA017",
+            title: "Setter Rule",
+            messageFormat: "You're missing setter method",
+            category: "Encapsulation",
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true
+        );
+
+        public static DiagnosticDescriptor GotoBanRule= new DiagnosticDescriptor(
+            id: "CA018",
+            title: "Goto Ban Rule",
+            messageFormat: "Goto usage is considered a bad practice",
+            category: "Code Conventions",
+            defaultSeverity: DiagnosticSeverity.Info,
+            isEnabledByDefault: true
+        );
+
+        public static DiagnosticDescriptor VarBanRule = new DiagnosticDescriptor(
+            id: "CA019",
+            title: "Var Ban Rule",
+            messageFormat: "Using var instead of type is considered a bad practice",
+            category: "Code Conventions",
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true
         );
     }
 
